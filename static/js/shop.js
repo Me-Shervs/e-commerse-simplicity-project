f8abbd297a8cc9097294bd34f9d941945cde27fb
Shervie <shervie123456789@gmail.com>
2026-03-08 19:18:09 +0800

    fixed home & shop link

--- static/js/shop.js
+++ static/js/shop.js
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
   if (cartIcon) {
     cartIcon.addEventListener("click", () => {
       
-      window.location.href = "../template/cart.html";
+      window.location.href = "template/cart.html";
     });
   }
 
